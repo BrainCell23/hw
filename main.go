0b331b60251427ddf59c114fb136bdda7cee778f
jenkinsbrainx <51398916+jenkinsbrainx@users.noreply.github.com>
2019-08-09 20:55:28 +0100

    Update main.go
    More meaningful title !

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := ":HW=HelloWorld: Jenkins X golang http example"
+	title := "The land of exploring Jenkins-x"
 
 	from := ""
 	if r.URL != nil {
